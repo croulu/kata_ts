d2a17d024365c9dd4f54dfae3e851c4910cf3127
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-13 00:03:21 +0100

    countOpenBraces et countCloseBraces

--- src/validBraces.spec.ts
+++ src/validBraces.spec.ts
@@ -1,4 +1,4 @@
-import {countOpenBraces, validBraces } from './validBraces';
+import {countOpenBraces, countCloseBraces, validBraces } from './validBraces';
 
 describe("should handle basic tests", function() {
 
@@ -16,14 +16,23 @@ describe("should handle basic tests", function() {
     });
 
     describe("countOpenBraces", function() {
-        const givenBraces:string = "((";
+        const givenBraces:string = "(((((";
         const givenBrace:string = "(";
-        const expected:number = 2;
+        const expected:number = 5;
         it (`given ${givenBraces} should return ${expected}`, ()=> {
             const sut = countOpenBraces(givenBraces, givenBrace);
             expect(sut).toStrictEqual(expected);
         });
+    });
 
+    describe("countCloseBraces", function() {
+        const givenBraces:string = "))))))))";
+        const givenBrace:string = ")";
+        const expected:number = 8;
+        it (`given ${givenBraces} should return ${expected}`, ()=> {
+            const sut = countCloseBraces(givenBraces, givenBrace);
+            expect(sut).toStrictEqual(expected);
+        });
     });
 
 });
--- src/validBraces.ts
+++ src/validBraces.ts
@@ -2,10 +2,19 @@ export function validBraces(braces: string): boolean {
     return false;
 }
 
-export function countOpenBraces(braces:string, brace:string):number {
+export function countOpenBraces(braces:string, braceToCount:string):number {
     let numberOfBrace = 0;
-    for (let element of braces) {
-        if (element == brace) numberOfBrace++;
+    for (let brace of braces) {
+        if (brace == braceToCount) numberOfBrace++;
     }
     return numberOfBrace;
 }
+
+export function countCloseBraces(braces:string, braceToCount:string):number {
+    let numberOfBrace = 0;
+    for (let brace of braces) {
+        if (brace == braceToCount) numberOfBrace++;
+    }
+    return numberOfBrace;
+}
+
