110e68e9caf1960d748df42bcef4d766094d196e
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-12 23:04:24 +0100

    init validBraces

---
+++ src/validBraces.spec.ts
@@ -0,0 +1,18 @@
+import { validBraces } from './validBraces';
+
+describe("solution", function() {
+    describe("should handle basic tests", function() {
+        it("should return true for ()", ()=> {
+            const given = "()";
+            const expected = true;
+            const sut = validBraces(given);
+            expect(sut).strictEqual(expected);
+        })
+        it("should return false for [(])", ()=> {
+            const given = "[(])";
+            const expected = false;
+            const sut = validBraces(given);
+            expect(sut).strictEqual(expected);
+        });
+    });
+});
